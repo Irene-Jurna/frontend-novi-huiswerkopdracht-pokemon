8c08b584204cde5b9ae0b17ddb5cdcb6eefd40bf
Irene Jurna <irene.jurna@gmail.com>
2022-09-29 21:15:14 +0200

    huiswerkopdracht t/m stap 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Pokemon from "./components/Pokemon";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [endpoint, setEndpoint] = useState('');
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await axios.get("https://pokeapi.co/api/v2/pokemon");
+                console.log(result.data.results);
+                setEndpoint(result.data.results);
+
+            } catch(error) {
+                console.error(error);
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div className = "pokemon-set">
+            {endpoint && endpoint.map((poke) => {
+                return <Pokemon name={poke.name} url={poke.url} key={poke.name}/>
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+function Pokemon( {name, url} ) {
+const [pokemon, setPokemon] = useState('');
+
+    useEffect(() => {
+        async function fetchPokemons() {
+            try {
+                const response = await axios.get(`${url}`);
+                console.log(response.data);
+                setPokemon(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchPokemons();
+    }, []);
+
+    return (
+        <section
+            className="pokemon-card"
+        >
+            {pokemon && <>
+                <h4>{pokemon.species.name}</h4>
+                <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.species.name} className="pokemon-image"/>
+                <p>Moves: {pokemon.moves.length}</p>
+                <p>Weight: {pokemon.weight}</p>
+                <p>Abilities:</p>
+                <div>{pokemon.abilities.map((ability) => {
+                    return <ul className="ability-list">
+                        <li key={name}>{ability.ability.name}</li>
+                    </ul>
+                })}</div>
+            </>
+            }
+        </section>
+    );
+}
+
+export default Pokemon;
